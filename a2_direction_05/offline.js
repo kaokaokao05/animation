5b4d91e6b3c0e265f97172ebae0ff6f4fa4c4ab8
kaokaokao05 <s40509@bj.ac.th>
2024-12-04 12:42:37 +0700

    Add files via upload

---
+++ a2_direction_05/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1733290900,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/collideobject-sheet0.png",
+		"images/playeranimations-sheet0.png",
+		"images/playeranimations-sheet1.png",
+		"images/player-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
